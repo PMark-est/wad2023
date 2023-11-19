ba9ecc46f4f9679a16e65433eeaa7f60fcbf6a2b
raya-mez <108145988+raya-mez@users.noreply.github.com>
2023-11-19 18:37:32 +0200

    Update SignUp.vue
    Imported sign up form component. 
    Header and footer components are commented out because they are still empty and because an error message indicated that the names of components should be multi-word.

--- wad2023vue/src/views/SignUp.vue
+++ wad2023vue/src/views/SignUp.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="signUp">
-    <h1>This is the sign up page</h1>
+    <!-- <header /> -->
+    <sign-up-form />
+    <!-- <footer /> -->
   </div>
 </template>
+
+<script>
+import SignUpForm from "@/components/SignUpForm.vue";
+// import Header from "@/components/Header.vue";
+// import Footer from "@/components/Footer.vue";
+export default {
+  name: "SignUp",
+  components: {
+    SignUpForm,
+    // Header,
+    // Footer,
+  },
+};
+</script>
