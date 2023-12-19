45cf79381c3ab90694c1aeabcf4ace283a803249
raya-mez <108145988+raya-mez@users.noreply.github.com>
2023-12-19 17:54:02 +0200

    Update SignUp.vue for

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -1,23 +1,93 @@
 <template>
-  <main class="signUpForm">
-    <sign-up-form />
-  </main>
+  <div class="form">
+    <h3>SignUp</h3>
+    <label for="email">Email</label>
+    <input type="email" name="email"  required v-model="email">
+    <label for="password">Password</label>
+    <input type="password" name="password" required v-model="password">
+    <button @click="SignUp" class="SignUp">SignUp</button>
+  </div>
 </template>
 
 <script>
-import SignUpForm from "@/components/SignUpForm.vue";
 export default {
-  name: "SignUp",
-  components: {
-    SignUpForm,
+name: "SignUp", 
+
+data: function() {
+    return {
+   email: '',
+   password: '',
+  }
   },
-};
+  methods: {
+
+
+SignUp() {
+      var data = {
+        email: this.email,
+        password: this.password
+      };
+      fetch("http://localhost:3000/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+          credentials: 'include', // enable cookies
+          body: JSON.stringify(data),
+      })
+      .then((response) => response.json())
+      .then((data) => {
+      console.log(data);
+      this.$router.push("/");
+      })
+      .catch((e) => {
+        console.log(e);
+        console.log("error");
+      });
+    },
+  }, 
+  }
 </script>
-<style lang="scss">
-.signUpForm {
+
+<style lang="scss" scoped>
+.form {
+  width: 420px;
+  margin: auto;
+  background: rgb(167, 154, 154);
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+h3 {
+  text-align: center;
+  color: rgb(8, 110, 110);
+}
+label {
+  color: rgb(8, 110, 110);
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.8em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input {
   display: block;
-  margin: 10px 30px;
-  padding: 15px 15px;
-  background-color: lightgoldenrodyellow;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid white;
+  color: blue;
+}
+button {
+  background: rgb(8, 110, 110);
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+  align-items: center;
+  text-align: center;
 }
 </style>
