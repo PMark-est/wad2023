a4f5356b6f0e4b9566bc22e252671e35429aee3b
raya-mez <108145988+raya-mez@users.noreply.github.com>
2023-12-19 19:32:04 +0200

    Added router link to Contacts page

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/signUp">Sign up</router-link>
+      <router-link to="/contacts">Contacts</router-link>
     </nav>
   </header>
 </template>
